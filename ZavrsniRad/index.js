584d430445c88d00e8944936e4da40293c38d465
LukaBozo <luka.bozanovic2@gmail.com>
2026-03-28 18:20:56 +0100

    zavrsni

--- ZavrsniRad/index.js
+++ ZavrsniRad/index.js
@@ -6,3 +6,14 @@ if (toggleButton && nav) {
     nav.classList.toggle('show');
   });
 }
+
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+if (slides.length > 0) {
+  setInterval(() => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+  }, 10000);
+}
